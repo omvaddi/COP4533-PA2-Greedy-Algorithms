f748c2649d36eb89ad070ec5f4f0b7e2005ca786
Om Vaddi <om.r.vaddi@gmail.com>
2026-03-04 15:45:44 -0500

    implemented lru cache + fifo cache optimization

--- src/cache.py
+++ src/cache.py
@@ -1,4 +1,5 @@
 from collections import deque
+from collections import OrderedDict
 
 def read_input(file_path):
     with open(file_path, 'r') as file:
@@ -6,26 +7,53 @@ def read_input(file_path):
         requests = list(map(int, file.readline().split()))
     return k, m, requests
 
-def fifo_cache(file_path):
-    k, m, requests = read_input(file_path)
 
+def call_caches(file_path):
+    k, m, requests = read_input(file_path)
+    
+    fifo_misses = fifo_cache(k, requests)
+    lru_misses = lru_cache(k, requests)
+    
+    print(f"FIFO : {fifo_misses}")
+    print(f"LRU : {lru_misses}")
+
+
+def fifo_cache(k, requests):
     cache = deque()
+    cache_set = set()
     misses = 0
     
     for request in requests:
         if request not in cache:
-            if len(cache) < k:
-                cache.append(request)
-            else:
-                cache.popleft()
-                cache.append(request)
+            if len(cache) == k:
+                oldest = cache.popleft()
+                cache_set.remove(oldest)
+            
+            cache.append(request)
+            cache_set.add(request)
             misses += 1
     
     return misses
 
 
+def lru_cache(k, requests):
+    cache = OrderedDict()
+    misses = 0
+
+    for request in requests:
+        if request in cache:
+            cache.move_to_end(request)
+        else:
+            if(len(cache) == k):
+                cache.popitem(last = False)
+            cache[request] = True
+            misses += 1
+
+    return misses        
+
+                    
 def main():
-    print(fifo_cache('tests/test1.txt'))
+    call_caches('tests/test1.txt')
 
 if __name__ == "__main__":
     main()
